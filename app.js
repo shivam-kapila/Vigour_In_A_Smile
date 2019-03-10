fc3a5637ca290959313fa86eb082f82eed6f87f6
shivam-kapila <shivamkapila4@gmail.com>
2019-03-10 13:23:29 +0530

    Likes and COmments done

--- app.js
+++ app.js
@@ -15,10 +15,18 @@ app.use(methodOveride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
+    name: String,
+    email: String,
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    likes: Number,
+    created: {type: Date, default: Date.now},
+    comments:[ {
+        name: String,
+        comment: String
+    }
+]
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
@@ -49,6 +57,11 @@ app.get("/blogs/new", function(req, res){
 app.post("/blogs", function (req, res) {
     //create blog
     req.body.blog.body = req.sanitize(req.body.blog.body);
+    // req.body.blog.name = req.user.name";
+    req.body.blog.name ="Shivam";
+    // req.body.blog.email = req.user.email;
+    req.body.blog.email ="shivamkapila4@gmail.com";
+    req.body.blog.likes = 0;
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -111,6 +124,39 @@ app.get("/login",function(req, res){
     res.render("login");
 }
 );
+
+// LIKE ROUTE
+
+app.post("/blogs/like/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, blog){
+        if(err){
+            console.log(err);
+        }
+        else {
+            blog.likes = blog.likes + 1;
+            blog.save();
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// COMMENT ROUTE 
+
+
+app.post("/blogs/comment/:id", function (req, res) {
+    Blog.findById(req.params.id, function (err, blog) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // req.body.comment.name = req.user.name;
+            req.body.comment.name = "Shivam";
+            blog.comments.push(req.body.comment);
+            blog.save();
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
     console.log("SERVER IS RUNNING!");
